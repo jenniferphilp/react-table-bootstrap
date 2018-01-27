96ba15b82308d1246de1da9b36c5924359720a57
jenniferphilp <jennifer.philp@gmail.com>
2018-01-27 17:53:05 -0500

    create data.js; change Sum function

--- app/src/App.js
+++ app/src/App.js
@@ -2,38 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import { Row, Col, Table } from 'react-bootstrap';
 import _ from 'lodash';
+import { data } from './data.js';
 
-const data = [
-  { name: 'Veggie Burger', menuCategory: 'Burgers', salesCategory: 'Food', menuPage: 'Dinner', salesRevenue: 40, quantitySold: 4 },
-  { name: 'Spicy Burger', menuCategory: 'Burgers', salesCategory: 'Food', menuPage: 'Dinner', salesRevenue: 20, quantitySold: 2 },
-  { name: 'Portobello Sandwich', menuCategory: 'Sandwiches', salesCategory: 'Food', menuPage: 'Dinner', salesRevenue: 0, quantitySold: 0 },
-  { name: 'Pabst Blue Ribbon', menuCategory: 'Beers', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 100, quantitySold: 50 },
-  { name: 'Nickel Brook Head Stock', menuCategory: 'Beers', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 96, quantitySold: 12 },
-  { name: 'Laphroaig', menuCategory: 'Scotches', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 0, quantitySold: 0 },
-  { name: 'Lagavulin 16', menuCategory: 'Scotches', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 60, quantitySold: 4 },
-  { name: 'Johnny Blue', menuCategory: 'Scotches', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 20, quantitySold: 1 },
-  { name: 'Johnny Black', menuCategory: 'Scotches', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 0, quantitySold: 0 },
-  { name: 'Hops and Robbers', menuCategory: 'Beers', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 121, quantitySold: 13 },
-  { name: 'Granite Peculiar', menuCategory: 'Beers', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 50, quantitySold: 6 },
-  { name: 'Glenmorangie', menuCategory: 'Scotches', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 0, quantitySold: 0 },
-  { name: 'Glenfiddich', menuCategory: 'Scotches', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 0, quantitySold: 0 },
-  { name: 'GLB Canuck', menuCategory: 'Beers', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 9, quantitySold: 1 },
-  { name: 'DAB', menuCategory: 'Beers', salesCategory: 'Alcohol', menuPage: 'Drinks', salesRevenue: 438, quantitySold: 91 },
-  { name: 'Club Sandwich', menuCategory: 'Sandwiches', salesCategory: 'Food', menuPage: 'Dinner', salesRevenue: 18, quantitySold: 2 },
-  { name: 'Ceasar Wrap', menuCategory: 'Sandwiches', salesCategory: 'Food', menuPage: 'Dinner', salesRevenue: 45, quantitySold: 5 },
-  { name: 'Bacon Burger', menuCategory: 'Burgers', salesCategory: 'Food', menuPage: 'Dinner', salesRevenue: 60, quantitySold: 6 },
-]
+
 
 class App extends Component {
   constructor(props){
     super(props);
 
         this.state = {
+            
         data: data,
         isSorted: [false, false, false, false, false, false]
         }
       
-
         this.sortItem = this.sortItem.bind(this);
 
     }
@@ -46,11 +28,11 @@ sortItem = (value, index) => {
     //make some copies...
     let isSortedCopy = [...this.state.isSorted];
     let data = this.state.data;
-    let copy = Object.assign({}, data);
+    let dataCopy = Object.assign({}, data);
     
     //use lodash to sort, based on value of isSortedItem (boolean)
     let sorted; 
-    isSortedItem ? sorted =  _.orderBy(copy, [value], ['desc']) : sorted = _.sortBy(copy, o => o[value]);
+    isSortedItem ? sorted =  _.orderBy(dataCopy, [value], ['desc']) : sorted = _.sortBy(dataCopy, o => o[value]);
 
     //change the boolean value of isSorted, at the appropriate index
     isSortedCopy[index] = !isSortedItem;
@@ -64,8 +46,8 @@ sortItem = (value, index) => {
 
 render() {
 
-    const dataProducts = (this.state.data).map((item, index) => {
-        return (<tr key={index}>
+    const dataProducts = (this.state.data).map((item, id) => {
+        return (<tr key={id}>
                 <td>{item.name}</td>
                 <td>{item.menuCategory}</td>
                 <td>{item.salesCategory}</td>
@@ -92,8 +74,8 @@ render() {
                     <tfoot>
                         <tr>
                             <td colSpan="4" className="subtotal sum sumTitle">Sum</td>
-                            <SumSalesRevenue /> 
-                            <SumQuantitySold /> 
+                            <Sum type="salesRevenue"/> 
+                            <Sum type="quantitySold" /> 
                         </tr>
                     </tfoot>            
                     <tbody>
@@ -105,18 +87,12 @@ render() {
       </div>
 )}}
 
-//keep these stateless for now, unless we do the filter feature. 
-const SumSalesRevenue = () => { 
-    let sum = data.reduce((prevVal, item) => prevVal + item.salesRevenue, 0)
-    return (<td className="sum">{sum}</td>)
-};
- 
-const SumQuantitySold = () => {
-    let sum = data.reduce((prevVal, item) => prevVal + item.quantitySold, 0)
+const Sum = ({ type }) => { 
+    let sum = data.reduce((prevVal, item) => prevVal + item[type], 0)
+    debugger; 
     return (<td className="sum">{sum}</td>)
 };
 
-//should this be a class? Does it need to be? 
 const Header = (props) => {
     let headerNames = Object.keys(...data);
         return (
